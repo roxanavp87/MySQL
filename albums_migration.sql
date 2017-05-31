3ab4ecf2a9297eaf096f7b77de3bc72866665850
roxanavp87 <roxanavp87@gmail.com>
2017-05-31 11:10:54 -0500

    - created table almbums

---
+++ albums_migration.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  arstist VARCHAR(25) NOT NULL,
+  name VARCHAR(25) NOT NULL,
+  release_date YEAR,
+  sales FLOAT UNSIGNED,
+  genre VARCHAR(10)
+);
