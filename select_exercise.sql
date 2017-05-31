9df7d719da90ebfc6640570e2111c59e9887b652
roxanavp87 <roxanavp87@gmail.com>
2017-05-31 14:58:28 -0500

    - select the name of all albums in the rock genre

--- select_exercise.sql
+++ select_exercise.sql
@@ -2,5 +2,19 @@ USE codeup_test_db;
 
 -- queries
 
--- select the name of all albums by Pink Floyd.
-select name from albums where artist = 'Pink Floyd';
+-- select name of all albums by Pink Floyd.
+SELECT 'name of all albums by Pink Floyd' AS 'Info';
+SELECT name FROM albums WHERE artist = 'Pink Floyd';
+
+-- select year Falling into You was released
+SELECT 'year Falling into You was released' AS 'Info';
+SELECT release_date FROM albums WHERE name = 'Falling into You';
+
+SELECT 'genre for Nevermind' AS 'Info';
+SELECT genre FROM albums WHERE name = 'Nevermind';
+
+SELECT 'Which albums were released in the 1990s' AS 'Info';
+SELECT name FROM albums WHERE release_date = 1990;
+
+SELECT 'All the albums in the rock genre' AS 'Info';
+SELECT name, genre FROM albums WHERE genre = 'Rock';
