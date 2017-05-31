e108a8251b3d54d4f14abaed2918bf6e42803846
roxanavp87 <roxanavp87@gmail.com>
2017-05-31 15:46:18 -0500

    - Which albums had less than 20 million certified sales

--- select_exercise.sql
+++ select_exercise.sql
@@ -14,7 +14,15 @@ SELECT 'genre for Nevermind' AS 'Info';
 SELECT genre FROM albums WHERE name = 'Nevermind';
 
 SELECT 'Which albums were released in the 1990s' AS 'Info';
-SELECT name FROM albums WHERE release_date = 1990;
+SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 
 SELECT 'All the albums in the rock genre' AS 'Info';
 SELECT name, genre FROM albums WHERE genre = 'Rock';
+
+SELECT *
+FROM albums
+WHERE INSTR(genre, 'Rock') > 0;
+
+SELECT 'Which albums had less than 20 million certified sales' AS 'Info';
+SELECT name
+FROM albums WHERE sales<20;
