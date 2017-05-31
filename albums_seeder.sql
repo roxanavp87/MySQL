3443cb2020daa16cfed8dbaa1434821abab15039
roxanavp87 <roxanavp87@gmail.com>
2017-05-31 12:14:07 -0500

    - inserted 2 rows in the table albums using a single INSERT

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -5,3 +5,7 @@ USE codeup_test_db;
 
 INSERT INTO albums(arstist, name, release_date, sales, genre) VALUE ('Michael Jackson', 'Thriller', 1982, 46.3, 'Pop, Rock');
 INSERT INTO albums(arstist, name, release_date, sales, genre) VALUE ('Pink Floyd', 'The Dark Side of the Moon', 1973, 24.2, 'Progressive rock');
+
+INSERT INTO albums(arstist, name, release_date, sales, genre)
+  VALUE ('Michael Jackson', 'Thriller', 1982, 46.3, 'Pop, Rock'),
+        ('Pink Floyd', 'The Dark Side of the Moon', 1973, 24.2, 'Progressive rock');
