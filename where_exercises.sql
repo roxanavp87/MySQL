b16ba745bae086383abb37b7d175227aa573d849
roxanavp87 <roxanavp87@gmail.com>
2017-06-01 11:59:51 -0500

    select: - Employees whose last name starts with 'E'

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,3 +1,6 @@
 
 SELECT 'Employees with first names ''Irena'', ''Vidya'', or ''Maya'' — 709 rows (Hint: Use IN).' AS '';
 SELECT * FROM employees.employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT 'Employees whose last name starts with ''E'' — 7,330 rows.' AS '';
+SELECT * FROM employees.employees WHERE last_name  LIKE 'E%';
