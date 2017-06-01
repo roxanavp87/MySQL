91106c580777ebde3c2027f0404255746bb6599e
roxanavp87 <roxanavp87@gmail.com>
2017-06-01 17:32:30 -0500

    - find the unique last names with a 'q' but not 'qu'. You may use either DISTINCT or GROUP BY

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -12,3 +12,9 @@ SELECT last_name
 FROM employees.employees
 WHERE last_name LIKE 'E%E'
 GROUP BY last_name;
+
+SELECT last_name
+FROM employees.employees
+WHERE  last_name LIKE '%q%'
+      AND  last_name NOT LIKE '%qu%'
+GROUP BY last_name;
