93c5be9b768740e22ea069689cb52eb008c0ecfb
roxanavp87 <roxanavp87@gmail.com>
2017-06-02 09:07:50 -0500

    - finded just the first 5 employees born on Christmas and hired in the 90s

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -3,5 +3,13 @@ USE employees;
 # List the first 10 distinct last name sorted in descending order
 SELECT last_name
 FROM employees.employees
-GROUP BY last_name DESC 
+GROUP BY last_name DESC
 LIMIT 10;
+
+# find just the first 5 employees born on Christmas and hired in the 90s
+SELECT first_name, last_name
+FROM employees.employees
+WHERE hire_date LIKE '199%'
+AND birth_date LIKE '%-12-25'
+LIMIT 5;
+
