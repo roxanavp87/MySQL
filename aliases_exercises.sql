e973e2bbf190eda6abc2b1078faf34960975d378
roxanavp87 <roxanavp87@gmail.com>
2017-06-02 12:06:35 -0500

    - Update the query to format full name to include the employee number so it is formatted as 'employee number - lastname, firstname'

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -5,3 +5,11 @@ FROM employees.employees
 LIMIT 10;
 
 
+# Update the query to format full name to include the employee number so it is formatted as
+# 'employee number - lastname, firstname'
+SELECT concat(emp_no, ' - ', last_name, ' ', first_name) AS full_name, birth_date as DOB
+FROM employees.employees
+LIMIT 10;
+
+
+
