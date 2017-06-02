3331100e383ff18574e9ebfbcd59a86f7d404588
roxanavp87 <roxanavp87@gmail.com>
2017-06-02 09:55:54 -0500

    - finded just the first 5 employees born on Christmas and hired in the 90s with offset 5

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -13,3 +13,16 @@ WHERE hire_date LIKE '199%'
 AND birth_date LIKE '%-12-25'
 LIMIT 5;
 
+SELECT first_name, last_name
+FROM employees.employees
+WHERE hire_date LIKE '199%'
+      AND birth_date LIKE '%-12-25'
+LIMIT 5 OFFSET 5;
+
+
+
+
+
+
+-- SELECT @@GLOBAL.sql_mode;
+-- STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION
