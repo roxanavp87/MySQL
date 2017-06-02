5b17255c18b92e4119b93ac432d113c17afb36f0
roxanavp87 <roxanavp87@gmail.com>
2017-06-02 11:38:01 -0500

    - find how many days employees hired in the 90s and born on Christmas have been working at the company

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,8 +1,9 @@
 # find the number of employees for each gender with those names
-SELECT COUNT(*), gender
+SELECT count(*), gender
 FROM employees.employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 GROUP BY gender;
+
 
 # combine first and last name together for employees whose names start and end with 'E' as a single column
 SELECT concat(first_name, ' ', last_name)
@@ -10,16 +11,8 @@ FROM employees.employees
 WHERE last_name  LIKE 'E%E';
 
 
-
-# reversing order
-
-SELECT * FROM employees.employees
-WHERE last_name  LIKE 'E%'
-ORDER BY emp_no DESC ;
-
-# Change the query for employees hired in the 90s and born on Christmas such
-# that the first result is the oldest employee who was hired last. It should be Khun Bernini.
-SELECT *
+# find how many days employees hired in the 90s and born on Christmas have been working at the company
+SELECT concat(first_name, ' ', last_name), datediff(curdate(), hire_date)
 FROM employees.employees
 WHERE hire_date LIKE '199%'
       AND birth_date LIKE '%-12-25'
