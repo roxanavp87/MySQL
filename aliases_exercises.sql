ebd71650e49cd5c23893d3ce404243573c3e95fa
roxanavp87 <roxanavp87@gmail.com>
2017-06-02 12:03:48 -0500

    - Add the date of birth for each employee as 'DOB' to the query

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,5 +1,7 @@
 
 # Return 10 employees in a result set named 'full_name' in the format of 'lastname, firstname' for each employee.
-SELECT concat(last_name, ' ', first_name) AS full_name
+SELECT concat(last_name, ' ', first_name) AS full_name, birth_date as DOB
 FROM employees.employees
 LIMIT 10;
+
+
