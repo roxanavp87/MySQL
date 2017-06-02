37b847ed8d5631f141d42cb338f29450a09da8a6
roxanavp87 <roxanavp87@gmail.com>
2017-06-02 11:48:59 -0500

    - employees with a 'q' in their last name but not 'qu' without repetitions

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -15,5 +15,12 @@ WHERE last_name  LIKE 'E%E';
 SELECT concat(first_name, ' ', last_name), datediff(curdate(), hire_date)
 FROM employees.employees
 WHERE hire_date LIKE '199%'
-      AND birth_date LIKE '%-12-25'
-ORDER BY birth_date, hire_date DESC;
+      AND birth_date LIKE '%-12-25';
+
+
+# employees with a 'q' in their last name but not 'qu' without repetitions
+SELECT count(DISTINCT first_name), last_name
+FROM employees.employees
+WHERE last_name LIKE '%q%'
+      AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
