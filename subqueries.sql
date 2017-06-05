0f493009f7c0b7df558905fcf4fceecc1b7fdd26
roxanavp87 <roxanavp87@gmail.com>
2017-06-05 09:15:11 -0500

    Sub queries exercises - Find all the department managers that are female

--- subqueries.sql
+++ subqueries.sql
@@ -27,10 +27,26 @@ WHERE emp_no IN (
 )
 group by emp_no;
 
-SELECT concat(e.first_name, ' ', e.last_name), t.title
+SELECT concat(e.first_name, ' ', e.last_name) AS name, t.title
 FROM employees as e
 JOIN titles as t ON e.emp_no = t.emp_no
 WHERE first_name = 'Aamod'
 GROUP BY e.emp_no;
 
+# Find all the department managers that are female.
+SELECT *
+FROM dept_manager
+WHERE to_date LIKE '9999%' AND emp_no IN (
+  SELECT emp_no
+  FROM employees
+  WHERE gender = 'F'
+);
 
+
+SELECT concat(first_name, ' ', last_name) AS name, emp_no
+FROM employees
+WHERE gender = 'F' AND emp_no IN (
+  SELECT emp_no
+  FROM dept_manager
+  WHERE to_date LIKE '9999%'
+);
